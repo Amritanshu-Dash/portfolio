d82e239de9d8dc9a412ec90b70851930cffbe353
amritanshu_dash <amritanshudash6677@gmail.com>
2024-02-29 00:19:21 +0530

    optimise left

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 
+import SocialProfiles from "./myComponents/socialProfiles";
+import { RiStackFill } from "react-icons/ri";
+import { IoLogoPython } from "react-icons/io5";
+import { PiProjectorScreenChartDuotone } from "react-icons/pi";
+
 import './App.css'
 function App() {
   return (
-    <div className="appBackground">
-      <div className="content">
-      </div>
-      <div className="sidebar"></div>
+
+
+    <div className="arrange">
+        <h1>Amritanshu Dash</h1>
+        <SocialProfiles/>
+        <p> 
+            Hey hey, I'm Amritanshu Dash, a fresh face in the software scene, rocking my final year of college! 
+            <br/>
+            Don't let the 'newbie' title fool you—I've got the passion and drive to rival the pros!
+        </p>
+
+        <p>
+          <h2>My Tech Stack <RiStackFill /></h2>
+          DSA and C/C++
+          <br/>
+          iOS Development and Swift.
+          <br/>
+          Web Technologies (HTML, CSS, JavaScript, React)
+          <br/>
+          Python <IoLogoPython />
+        </p>  
     </div>
   );
 }
 
-export default App;
+export default App; 
--- src/components/name/name.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import '../name/name.css'
-export default function name() {
-  return (
-    <div className='nameStyle'>
-        Amritanshu Dash
-    </div>
-  )
-}
