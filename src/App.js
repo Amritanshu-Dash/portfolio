3506d71274b6560a339e4ec5c385f50120e77da6
amritanshu_dash <amritanshudash6677@gmail.com>
2024-02-29 21:29:15 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
-
 import SocialProfiles from "./myComponents/socialProfiles";
+import TechStack from "./myComponents/techStack";
 import { RiStackFill } from "react-icons/ri";
-import { IoLogoPython } from "react-icons/io5";
-import { PiProjectorScreenChartDuotone } from "react-icons/pi";
+import './App.css';
 
-import './App.css'
 function App() {
   return (
-
-
+    
     <div className="arrange">
-        <h1>Amritanshu Dash</h1>
-        <SocialProfiles/>
-        <p> 
-            Hey hey, I'm Amritanshu Dash, a fresh face in the software scene, rocking my final year of college! 
-            <br/>
-            Don't let the 'newbie' title fool you—I've got the passion and drive to rival the pros!
-        </p>
 
-        <p>
-          <h2>My Tech Stack <RiStackFill /></h2>
-          DSA and C/C++
-          <br/>
-          iOS Development and Swift.
-          <br/>
-          Web Technologies (HTML, CSS, JavaScript, React)
-          <br/>
-          Python <IoLogoPython />
-        </p>  
+      <h1>Amritanshu Dash</h1>
+      <SocialProfiles />
+
+      <p>
+        Hey hey, I'm Amritanshu Dash, a fresh face in the software scene, rocking my final year of college!
+        <br />
+        Don't let the 'newbie' title fool you—I've got the passion and drive to rival the pros!
+      </p>
+
+      <h1>My Tech Stack <RiStackFill /></h1>
+      <TechStack />
+
     </div>
   );
 }
